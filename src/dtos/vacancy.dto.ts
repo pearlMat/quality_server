0b85d84f023902d4f95e6148c9e1163ecd657100
ptakang <pearllyt@gmail.com>
2023-04-24 17:24:39 +0100

    created vacancy dto file

---
+++ src/dtos/vacancy.dto.ts
@@ -0,0 +1,27 @@
+import { IsString } from 'class-validator';
+
+export class CreateVacancyDto {
+  @IsString()
+  public job_ref: string;
+
+  @IsString()
+  public location: string;
+  @IsString()
+  public pay_rate: string;
+  @IsString()
+  public work_patterns: string;
+
+  @IsString()
+  public description: string;
+
+  @IsString()
+  public phone: string;
+  @IsString()
+  public driver_required: string;
+
+  @IsString()
+  public essential_experience: string;
+
+  @IsString()
+  public desirable_experience: string;
+}
