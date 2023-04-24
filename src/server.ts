43ac56a91f1aa0d355e934730f9dda1018562245
ptakang <pearllyt@gmail.com>
2023-04-24 19:25:40 +0100

    added vacancy and applicant route to main route

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,12 @@ import AuthRoute from '@routes/auth.route';
 import IndexRoute from '@routes/index.route';
 import UsersRoute from '@routes/users.route';
 import ContactsRoute from './routes/contacts.route';
+import ApplicantsRoute from './routes/applicants.route';
+import VacancyRoute from './routes/vacancy.route';
 import validateEnv from '@utils/validateEnv';
 
 validateEnv();
 
-const app = new App([new IndexRoute(), new UsersRoute(), new ContactsRoute(), new AuthRoute()]);
+const app = new App([new IndexRoute(), new UsersRoute(), new ContactsRoute(), new AuthRoute(), new ApplicantsRoute(), new VacancyRoute()]);
 
 app.listen();
