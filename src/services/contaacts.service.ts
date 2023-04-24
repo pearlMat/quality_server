92d48c9daf2537b2ffc411bb31bd36dd03f94776
ptakang <pearllyt@gmail.com>
2023-04-24 10:11:48 +0100

    created contact service

---
+++ src/services/contaacts.service.ts
@@ -0,0 +1,31 @@
+import { PrismaClient, Contact } from '@prisma/client';
+import { CreateContactDto } from '@/dtos/contacts.dto';
+import { HttpException } from '@exceptions/HttpException';
+import { isEmpty } from '@utils/util';
+
+class ContactService {
+  public contacts = new PrismaClient().contact;
+
+  public async findAllContacts(): Promise<Contact[]> {
+    const allContacts: Contact[] = await this.contacts.findMany();
+    return allContacts;
+  }
+
+  public async findContactById(ContactId: number): Promise<Contact> {
+    if (isEmpty(ContactId)) throw new HttpException(400, 'ContactId is empty');
+
+    const findContact: Contact = await this.contacts.findUnique({ where: { id: ContactId } });
+    if (!findContact) throw new HttpException(409, "Contact doesn't exist");
+
+    return findContact;
+  }
+
+  public async createContact(ContactData: CreateContactDto): Promise<Contact> {
+    if (isEmpty(ContactData)) throw new HttpException(400, 'ContactData is empty');
+
+    const createContactData: Contact = await this.contacts.create({ data: { ...ContactData } });
+    return createContactData;
+  }
+}
+
+export default ContactService;
