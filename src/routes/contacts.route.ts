b743bfe314aa66ab9f514b52581db5ad4a9cf9e2
ptakang <pearllyt@gmail.com>
2023-04-24 10:36:15 +0100

    created contact routes

---
+++ src/routes/contacts.route.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import ContactsController from '@/controllers/contacts.controller';
+import { CreateContactDto } from '@/dtos/contacts.dto';
+import { Routes } from '@interfaces/routes.interface';
+import validationMiddleware from '@middlewares/validation.middleware';
+
+class ContactsRoute implements Routes {
+  public path = '/contacts';
+  public router = Router();
+  public contactsController = new ContactsController();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.get(`${this.path}`, this.contactsController.getContacts);
+    this.router.get(`${this.path}/:id(\\d+)`, this.contactsController.getContactById);
+    this.router.post(`${this.path}`, validationMiddleware(CreateContactDto, 'body'), this.contactsController.createContact);
+  }
+}
+
+export default ContactsRoute;
